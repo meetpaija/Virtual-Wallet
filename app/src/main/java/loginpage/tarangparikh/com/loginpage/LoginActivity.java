4cdd41c42cbf7c67aae59ed0ad2e40765097d679
meetpaija <meet.paija@gmail.com>
2017-01-27 20:34:39 +0530

    commit5

--- app/src/main/java/loginpage/tarangparikh/com/loginpage/LoginActivity.java
+++ app/src/main/java/loginpage/tarangparikh/com/loginpage/LoginActivity.java
@@ -1,11 +1,10 @@
 package loginpage.tarangparikh.com.loginpage;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +34,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         firebaseAuth=FirebaseAuth.getInstance();
         if(firebaseAuth.getCurrentUser()!=null){
             finish();
-            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+            startActivity(new Intent(getApplicationContext(),DashBoardActivity.class));
         }
 
         editTextEmail=(EditText)findViewById(R.id.editTextEmail1);
@@ -70,7 +69,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         progressDialog.dismiss();
                         if(task.isSuccessful()){
                             finish();
-                            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                            startActivity(new Intent(getApplicationContext(),DashBoardActivity.class));
                         }
 
 
---
+++ app/src/main/java/loginpage/tarangparikh/com/loginpage/notification_fragment.java
@@ -0,0 +1,23 @@
+package loginpage.tarangparikh.com.loginpage;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Meet Paija on 27-01-2017.
+ */
+
+public class notification_fragment extends Fragment{
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        view= inflater.inflate(R.layout.notification_layout,container,false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
